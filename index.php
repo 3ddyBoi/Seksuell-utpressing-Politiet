dcf9f85eca1048a8895806dc4bc589b236d3021d
3ddyBoi <edvardhoiby@outlook.com>
2020-12-02 20:01:40 +0100

    v1.0
    02.12.2020
    20:01

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+
+<html lang="nb-NO">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css">
+  <link rel="shortcut icon" type="image/svg" href="assets/ico-riksvåpen.svg"/>
+  <!--SEO-->
+  <title>Seksuell utpressing</title>
+  <meta name="description" content="Seksuell utpressing">
+  <meta name="author" content="Edvard Høiby">
+  <!--font-->
+  <link rel="stylesheet" href="https://use.typekit.net/drz3hva.css">
+</head>
+
+<body>
+  <h1 class="main-h">Seksuell utpressing på internett</h1>
+
+  <!--JS-->
+  <script src="scripts.js"></script>
+</body>
+</html>
