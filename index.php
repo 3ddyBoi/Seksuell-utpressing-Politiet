963b8663cea762069ac5dfa42d54e2a1f3b9900e
3ddyBoi <70267476+3ddyBoi@users.noreply.github.com>
2020-12-03 15:20:19 +0100

    v1.1
    12/03/2020
    3:20 PM

--- index.php
+++ index.php
@@ -4,19 +4,55 @@
 <head>
   <meta charset="utf-8">
   <link rel="stylesheet" href="style.css">
-  <link rel="shortcut icon" type="image/svg" href="assets/ico-riksvåpen.svg"/>
+  <link rel="shortcut icon" type="image/svg" href="assets/riksvapen-yellow.svg"/>
   <!--SEO-->
   <title>Seksuell utpressing</title>
   <meta name="description" content="Seksuell utpressing">
   <meta name="author" content="Edvard Høiby">
-  <!--font-->
+  <!--Font-->
   <link rel="stylesheet" href="https://use.typekit.net/drz3hva.css">
 </head>
 
 <body>
-  <h1 class="main-h">Seksuell utpressing på internett</h1>
+ <nav class="header">
+    <ul class="ul">
+          <li class="h-li-su"><a class="h-a-su" href="index.php">Seksuell utpressing</a></li>
+          <a class="h-a-riksvapen" href="https://www.politiet.no/" target="blank"><img class="h-riksvapen" src="assets/riksvapen-header.svg" alt="Norges riksvåpen"></a>
+          <li class="h-li-jsu"><a class="h-a-jsu" href="Utsatt-for-seksuell-utpressing">Jeg har blitt utsatt for seksuell utpressing</a></li>
+      </ul>
+ </nav>
+
+  <div class="top-side">
+    <div class="front-side">
+          <h2 class="main-h2">Stadig flere personer blir utsatt for seksuell utpressing på internett. Gjerningspersonen presser penger av offeret.</h2>
+          <h1 class="main-h1">Ditt verste mareritt!</h1>
+    </div>
+  </div>
+
+  <div class="side2">
+    
+  </div>
+
+  <div class="side3">
+
+  </div>
+
+  <footer class="footer">
+    <h2 class="f-ring">Ring politiet 02800</h2>
+    <h3 class="">Samtalepriser</h3>
+    <h3 class="">Tips politiet</h3>
+    <h3 class="">Kontakt politiet</h3>
+
+    <h2 class="">Nødnummer 112</h2>
+    <h3 class="">Send Nød-SMS</h3>
+
+    <h2 class="">Om nettsiden</h2>
+    <h3 class="">Personvern og informasjonskapslar</h3>
+    <h3 class="">Personvernerklæring</h3>
+    <h3 class="">Skoleprosjekt</h3>
+  </footer>
 
   <!--JS-->
-  <script src="scripts.js"></script>
+  <script src="script.js"></script>
 </body>
 </html>
