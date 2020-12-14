fef3c3526f91b840e7d398980a9a1da2b99c1bee
3ddyBoi <edvardhoiby@outlook.com>
2020-12-14 22:40:26 +0100

    v1.3.4
    14/12/2020
    10:40 PM

--- index.php
+++ index.php
@@ -25,98 +25,97 @@
   </head>
 
   <body>
-  <nav class="navbar">
-    <ul class="ul">
-        <li class="h-li-su"><a class="h-a-su-aktiv" href="index.php">Seksuell utpressing</a></li>
-        <a class="h-a-riksvapen" href="https://www.politiet.no/" target="blank"><img class="h-riksvapen" src="assets/riksvapen-white.svg" alt="Norges riksvåpen"></a>
-      <li class="h-li-jsu"><a class="h-a-jsu" href="utsatt-for-seksuell-utpressing.php">Jeg har blitt utsatt for seksuell utpressing</a></li>
-     </ul>
-  </nav>
-
-  <div class="top-side">
-    <div class="front-side">
-      <h2 class="main-h2">Stadig flere personer blir utsatt for seksuell utpressing på internett. Gjerningspersonen presser penger av offeret.</h2>
-      <h1 class="main-h1">Ditt verste mareritt!</h1>
-    </div>
-  </div>
-  
-  <div class="side-2">
-    <div class="gif-side-2">
-      <img class="gif-juletre" src="assets\arve-88ditherd.gif" alt="Dans">
-    </div>
-    <div id="poll">
-      <h2 class="poll-h2">Har du noen gang deltatt i utpressing før?</h2>
-      <form>
-        <h3 class="poll-h3">Ja, men jeg angrer!: </h3><input type="radio" class="poll-knapp" name="vote" value="0" onclick="getVote(this.value)">
-        <h3 class="poll-h3">Tror ikke det: </h3><input type="radio" name="vote" class="poll-knapp" value="1" onclick="getVote(this.value)">
-        <h3 class="poll-h3">Nei jeg er ikke så slem!:</h3><input type="radio" class="poll-knapp" name="vote" value="2" onclick="getVote(this.value)">
-      </form>
-    </div>
-  </div>
-
-  <div class="parallax">
-    <div class="parallax-img">
-      <h2 class="parallax-tekst">Den ukjente bruker chattetjenester. Det er ofte unge menn som blir lurt på denne måten.</h2>
-    </div>
-  </div>
-  
-  <div class="side-3">
-    <div class="gif-side-3">
-      <img class="gif-hypnotisere" src="assets\hypnotisere.gif" alt="hypnotisert-jente">
-    </div>
-    <div class="hypnotisere">
-      <h2 class="hypnotisere-h2">Ikke la deg hypnotisere!</h2>
-      <h3 class="hypnotisere-h3">Det er lett å bli lurt! Uten at ofrene vet det, blir det gjort opptak. Gjerningspersonen truer med å publisere opptaket dersom offeret ikke betaler penger. Det kan være krenkende bilder eller videoer med seksuell karakter.</h3>
-    </div>
-  </div>
-
-  <div class="videoene">
-    <div class="video1">
-      <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/uY3IarntDlo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </div>
-    <div class="video1-tekst">
-        <h2 class="video-h2">Tittel - Seksuell utpressing på nett</h2>
-        <h3 class="video-h3">Innhold i videoen - Seksuell utpressing med økonomisk motiv er en økende trend. Det er ofte organiserte kriminelle som står bak.</h3>
-    </div>
-    <div class="video2">
-      <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/52JYgQNGBWY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    </div>
-    <div class="video2-tekst">
-        <h2 class="video-h2">Tittel - SayNO! - En Europol-kampanje mot seksuell utnytting</h2>
-        <h3 class="video-h3">Innhold i videoen - Fra Europol sin kampanje "SayNO!" mot seksuell utnytting.</h3>
-    </div>
-  </div>
-
-  <div class="parallax-side4">
-    <div class="parallax-img-side4">
-      <div class="parallax-tekst-side4">
-        <h2 class="hypnotisere-h2">Har du blitt utsatt for seksuell utpressing før?</h2>
-        <h3 class="hypnotisere-h3"><a class="parallax-link" href="Utsatt-for-seksuell-utpressing.php">Trykk her for å lese hva du burde gjøre!</a></h3>
+    <nav class="navbar">
+        <ul class="ul">
+          <li class="h-li-su"><a class="h-a-su-aktiv" href="index.php">Seksuell utpressing</a></li>
+          <a class="h-a-riksvapen" href="https://www.politiet.no/" target="blank"><img class="h-riksvapen" src="assets/riksvapen-white.svg" alt="Norges riksvåpen"></a>
+          <li class="h-li-jsu"><a class="h-a-jsu" href="utsatt-for-seksuell-utpressing.html">Jeg har blitt utsatt for seksuell utpressing</a></li>
+      </ul>
+    </nav>
+    <div class="top-side">
+      <div class="front-side">
+        <h2 class="main-h2">Stadig flere personer blir utsatt for seksuell utpressing på internett. Gjerningspersonen presser penger av offeret.</h2>
+        <h1 class="main-h1">Ditt verste mareritt!</h1>
       </div>
     </div>
-  </div>
+    
+    <div class="side-2">
+      <div class="gif-side-2">
+        <img class="gif-juletre" src="assets\arve-88ditherd.gif" alt="Dans">
+      </div>
+      <div id="poll">
+        <h2 class="poll-h2">Har du noen gang deltatt i utpressing før?</h2>
+        <form>
+          <h3 class="poll-h3">Ja, men jeg angrer!: </h3><input type="radio" class="poll-knapp" name="vote" value="0" onclick="getVote(this.value)">
+          <h3 class="poll-h3">Tror ikke det: </h3><input type="radio" name="vote" class="poll-knapp" value="1" onclick="getVote(this.value)">
+          <h3 class="poll-h3">Nei jeg er ikke så slem!:</h3><input type="radio" class="poll-knapp" name="vote" value="2" onclick="getVote(this.value)">
+        </form>
+      </div>
+    </div>
+
+    <div class="parallax">
+      <div class="parallax-img">
+        <h2 class="parallax-tekst">Den ukjente bruker chattetjenester. Det er ofte unge menn som blir lurt på denne måten.</h2>
+      </div>
+    </div>
+    
+    <div class="side-3">
+      <div class="gif-side-3">
+        <img class="gif-hypnotisere" src="assets\hypnotisere.gif" alt="hypnotisert-jente">
+      </div>
+      <div class="hypnotisere">
+        <h2 class="hypnotisere-h2">Ikke la deg hypnotisere!</h2>
+        <h3 class="hypnotisere-h3">Det er lett å bli lurt! Uten at ofrene vet det, blir det gjort opptak. Gjerningspersonen truer med å publisere opptaket dersom offeret ikke betaler penger. Det kan være krenkende bilder eller videoer med seksuell karakter.</h3>
+      </div>
+    </div>
+
+    <div class="videoene">
+      <div class="video1">
+        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/uY3IarntDlo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      <div class="video1-tekst">
+          <h2 class="video-h2">Tittel - Seksuell utpressing på nett</h2>
+          <h3 class="video-h3">Innhold i videoen - Seksuell utpressing med økonomisk motiv er en økende trend. Det er ofte organiserte kriminelle som står bak.</h3>
+      </div>
+      <div class="video2">
+        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/52JYgQNGBWY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+      <div class="video2-tekst">
+          <h2 class="video-h2">Tittel - SayNO! - En Europol-kampanje mot seksuell utnytting</h2>
+          <h3 class="video-h3">Innhold i videoen - Fra Europol sin kampanje "SayNO!" mot seksuell utnytting.</h3>
+      </div>
+    </div>
+
+    <div class="parallax-side4">
+      <div class="parallax-img-side4">
+        <div class="parallax-tekst-side4">
+          <h2 class="hypnotisere-h2">Har du blitt utsatt for seksuell utpressing før?</h2>
+          <h3 class="hypnotisere-h3"><a class="parallax-link" href="utsatt-for-seksuell-utpressing.html">Trykk her for å lese hva du burde gjøre!</a></h3>
+        </div>
+      </div>
+    </div>
 
 
-  <footer class="footer">
+    <footer class="footer">
     <div class="d-footer">
       <li class="f-li f-li-h f-ring"><a class="f-a" href="tel:02800">Ring politiet 02800</a></li>
-      <li class="f-li f-li-p f-ring-samtale"><a class="f-a" href="https://www.politiet.no/kontakt-oss/ring-politiet/">Samtalepriser</a></li>
-      <li class="f-li f-li-p f-ring-tips"><a class="f-a" href="https://www.politiet.no/tjenester/tips-politiet/">Tips politiet</a></li>
-      <li class="f-li f-ring-kontakt"><a class="f-a" href="https://www.politiet.no/kontakt-oss/">Kontakt politiet</a></li>
+      <li class="f-li f-li-p f-ring-samtale"><a class="f-a" href="https://www.politiet.no/kontakt-oss/ring-politiet/" target="blank">Samtalepriser</a></li>
+      <li class="f-li f-li-p f-ring-tips"><a class="f-a" href="https://www.politiet.no/tjenester/tips-politiet/" target="blank">Tips politiet</a></li>
+      <li class="f-li f-ring-kontakt"><a class="f-a" href="https://www.politiet.no/kontakt-oss/" target="blank">Kontakt politiet</a></li>
 
       <li class="f-li f-li-h f-nummer"><a class="f-a" href="tel:112">Nødnummer 112</a></li>
-      <li class="f-li f-li-p f-nummer-sms"><a class="f-a" href="https://www.politiet.no/kontakt-oss/nod-sms/">Send Nød-SMS</a></li>
+      <li class="f-li f-li-p f-nummer-sms"><a class="f-a" href="https://www.politiet.no/kontakt-oss/nod-sms/" target="blank">Send Nød-SMS</a></li>
 
-      <li class="f-li f-li-h f-om-nettsiden"><a class="f-a" href="https://www.politiet.no/finn-ditt-politi/">Finn ditt politi</a></li>
-      <li class="f-li f-li-p f-om-peronvern-og-infomasjonskapsler"><a class="f-a" href="">Personvern og informasjonskapslar</a></li>
+      <li class="f-li f-li-h f-om-nettsiden"><a class="f-a" href="https://www.politiet.no/finn-ditt-politi/" target="blank">Finn ditt politi</a></li>
+      <li class="f-li f-li-p f-om-peronvern-og-infomasjonskapsler"><a class="f-a" href="informasjonskapsler.html">Informasjonskapsler</a></li>
       <li class="f-li f-li-p f-om-peronvernerklæring"><a class="f-a" href="terms.html">Personvernerklæring</a></li>
-      <li class="f-li f-li-p f-om-skoleprosjekt"><a class="f-a" href="">Skoleprosjekt</a></li>
+      <li class="f-li f-li-p f-om-skoleprosjekt"><a class="f-a" href="skoleprosjekt.html">Skoleprosjekt</a></li>
     </div>
   </footer>
-    
-  <div class="d-kopirett">
-    <p class="f-li f-kopirett">Kopirett © 2020 Edvard Høiby • Designet og utviklet av <a class="github" href="https://github.com/3ddyBoi" target="blank">3ddyBoi</a></p>
-  </div>
+      
+    <div class="d-kopirett">
+      <p class="f-li f-kopirett">Kopirett © 2020 Edvard Høiby • Designet og utviklet av <a class="github" href="https://github.com/3ddyBoi" target="blank">3ddyBoi</a></p>
+    </div>
 
 
     <!--JS-->
